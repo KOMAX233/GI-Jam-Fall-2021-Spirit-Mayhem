2f28492b2eb9a82bc2b38a75749aa0e171f5cb4b
rsoiffer <>
2021-11-27 01:55:10 -0500

    Refactored code to new folder, added projectile

---
+++ Assets/Code/Moves/ProjectileMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ProjectileMove : Move
+{
+    public Rigidbody2D projectilePrefab;
+
+    public float speed;
+    public float windup;
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            TryStartMove();
+        }
+
+        if (IsActive && Time.time > LastStartTime + windup)
+        {
+            var projectile = Instantiate(projectilePrefab);
+            var toMouse = player.MousePos - player.transform.position;
+            projectile.transform.position = player.transform.position;
+            projectile.velocity = speed * toMouse.normalized;
+            EndMove();
+        }
+    }
+}
---
+++ Assets/Code/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damage;
+    public float lifetime;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var health = other.rigidbody.GetComponentInParent<Health>();
+        if (health != null)
+        {
+            health.Damage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
